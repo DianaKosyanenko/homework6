d9fd2e4180c13b0885a37364b98b9188b1e309b2
Diana <kosyanenkodiana@yandex.ru>
2023-01-06 20:06:12 +0300

    rep

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*int [] CreateArray(int size){
+     int [] array = new int [size];  
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter your number under index {i}: ");
+        array [i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArrai(int [] array){
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+
+    Console.WriteLine();
+}
+
+int PositiveNumbers (int [] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Enter array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = CreateArray(size);
+ShowArrai(array);
+Console.WriteLine($"Positive number is {PositiveNumbers(array)} ");*/
+
+/*Console.Write("k1=: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2=: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1=: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2=: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = -(b1 - b2) / (k1 - k2);
+double y = (k1 * x) + b1;
+Console.WriteLine($"({x};{y})");*/
+
+
